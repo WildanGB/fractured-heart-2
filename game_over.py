c60a155aeb148213069350b5ec1c4f85bae4d97e
Mohammad-Mahtab <mahtab.prof.code@gmail.com>
2025-02-12 19:39:21 +0300

    fixed game_over.py

--- game_over.py
+++ game_over.py
@@ -43,16 +43,35 @@ class GameOverScreen:
         if self.game_over_text_pos < HEIGHT // 2:
             self.game_over_text_pos += 5
         else:
-            # Draw the buttons; highlight the selected button
-            restart_color = "yellow" if self.selected_button == 0 else "green"
-            quit_color = "yellow" if self.selected_button == 1 else "red"
-            pygame.draw.rect(self.screen, restart_color, self.restart_button)
-            pygame.draw.rect(self.screen, quit_color, self.quit_button)
 
-            # Render button text
+             # Render button text
             button_font = pygame.font.Font(UI_FONT, 40)  # Reduced font size
             restart_text = button_font.render("Restart", True, "black")
             quit_text = button_font.render("Quit", True, "black")
+
+            # # Draw the buttons; highlight the selected button
+            # restart_color = "white" if self.selected_button == 0 else "green"
+            # quit_color = "white" if self.selected_button == 1 else "red"
+            # pygame.draw.rect(self.screen, restart_color, self.restart_button)
+            # pygame.draw.rect(self.screen, quit_color, self.quit_button)
+
+            if self.selected_button == 0:
+                pygame.draw.rect(self.screen, "white", self.restart_button)
+                pygame.draw.rect(self.screen, "black", self.restart_button, 5)
+                restart_text = button_font.render("Restart", True, "black")
+            else:
+                pygame.draw.rect(self.screen, "black", self.restart_button)
+                restart_text = button_font.render("Restart", True, "white")
+
+
+            if self.selected_button == 1: 
+                pygame.draw.rect(self.screen, "white", self.quit_button)
+                pygame.draw.rect(self.screen, "black", self.quit_button, 5)
+                quit_text = button_font.render("Quit", True, "black")
+            else: 
+                pygame.draw.rect(self.screen, "black", self.quit_button)
+                quit_text = button_font.render("Quit", True, "white")
+
 
             # Properly center text within buttons
             restart_rect = restart_text.get_rect(center=self.restart_button.center)
