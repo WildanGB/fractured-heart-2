bd35ba6140eb38dc936bf5a2b4a7276b397751f5
WildanGB <wildanbinmasood1@gmail.com>
2025-02-16 13:50:29 +0300

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -2,26 +2,29 @@ import pygame
 from settings import *
 from entity import Entity
 from support import *
+from random import randint
 
+# Global cache for enemy sounds and animations
+sound_cache = {}
+animation_cache = {}
 
 class Enemy(Entity):
     def __init__(self, monster_name, pos, groups, obstacle_sprites, damage_player, trigger_death_particles, add_exp):
-
-        # general setup
+        # General setup
         super().__init__(groups)
         self.sprite_type = 'enemy'
 
-        # graphics setup
+        # Graphics setup
         self.import_graphics(monster_name)
         self.status = 'idle'
         self.image = self.animations[self.status][self.frame_index]
 
-        # movement
+        # Movement
         self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0, -10)
         self.obstacle_sprites = obstacle_sprites
 
-        # stats
+        # Stats
         self.monster_name = monster_name
         monster_info = monster_data[self.monster_name]
         self.health = monster_info['health']
@@ -33,7 +36,7 @@ class Enemy(Entity):
         self.notice_radius = monster_info['notice_radius']
         self.attack_type = monster_info['attack_type']
 
-        # player interaction
+        # Player interaction
         self.can_attack = True
         self.attack_time = None
         self.attack_cooldown = 400
@@ -41,12 +44,12 @@ class Enemy(Entity):
         self.trigger_death_particles = trigger_death_particles
         self.add_exp = add_exp
 
-        # invincibility timer
+        # Invincibility timer
         self.vulnerable = True
         self.hit_time = None
         self.invincibility_duration = 300
 
-        # sounds
+        # Sounds
         self.death_sound = pygame.mixer.Sound('../audio/death.wav')
         self.hit_sound = pygame.mixer.Sound('../audio/hit.wav')
         self.attack_sound = pygame.mixer.Sound(monster_info['attack_sound'])
@@ -54,27 +57,35 @@ class Enemy(Entity):
         self.hit_sound.set_volume(0.6)
         self.attack_sound.set_volume(0.6)
 
+        # Optimization: Update throttling
+        self.update_interval = randint(3, 5)
+        self.last_update = 0
+
     def import_graphics(self, name):
         self.animations = {'idle': [], 'move': [], 'attack': []}
         main_path = f'../graphics/monsters/{name}/'
         for animation in self.animations.keys():
             self.animations[animation] = import_folder(main_path + animation)
+            if not self.animations[animation]:
+                print(f"Warning: No frames loaded for {name} animation '{animation}'. Using placeholder.")
+                placeholder = pygame.Surface((TILESIZE, TILESIZE))
+                placeholder.fill((255, 0, 255))
+                self.animations[animation] = [placeholder]
+        # Optionally, cache animations if desired
+        # animation_cache[name] = self.animations
 
     def get_player_distance_direction(self, player):
         enemy_vec = pygame.math.Vector2(self.rect.center)
         player_vec = pygame.math.Vector2(player.rect.center)
         distance = (player_vec - enemy_vec).magnitude()
-
         if distance > 0:
             direction = (player_vec - enemy_vec).normalize()
         else:
             direction = pygame.math.Vector2()
-
         return (distance, direction)
 
     def get_status(self, player):
         distance = self.get_player_distance_direction(player)[0]
-
         if distance <= self.attack_radius and self.can_attack:
             if self.status != 'attack':
                 self.frame_index = 0
@@ -96,16 +107,13 @@ class Enemy(Entity):
 
     def animate(self):
         animation = self.animations[self.status]
-
         self.frame_index += self.animation_speed
         if self.frame_index >= len(animation):
             if self.status == 'attack':
                 self.can_attack = False
             self.frame_index = 0
-
         self.image = animation[int(self.frame_index)]
         self.rect = self.image.get_rect(center=self.hitbox.center)
-
         if not self.vulnerable:
             alpha = self.wave_value()
             self.image.set_alpha(alpha)
@@ -117,7 +125,6 @@ class Enemy(Entity):
         if not self.can_attack:
             if current_time - self.attack_time >= self.attack_cooldown:
                 self.can_attack = True
-
         if not self.vulnerable:
             if current_time - self.hit_time >= self.invincibility_duration:
                 self.vulnerable = True
