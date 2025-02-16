36c2fe38670f1fe58e831403115a2869fedb42a5
WildanGB <wildanbinmasood1@gmail.com>
2025-02-16 13:50:52 +0300

    Update level.py

--- level.py
+++ level.py
@@ -11,11 +11,10 @@ from enemy import Enemy
 from particles import AnimationPlayer
 from magic import MagicPlayer
 from upgrade import Upgrade
-
+from enemy_spawner import EnemySpawner  # Import the spawner
 
 class Level:
     def __init__(self):
-
         # get the display surface
         self.display_surface = pygame.display.get_surface()
         self.game_paused = False
@@ -29,16 +28,27 @@ class Level:
         self.attack_sprites = pygame.sprite.Group()
         self.attackable_sprites = pygame.sprite.Group()
 
-        # sprite setup
+        # Collision grid setup (if used)
+        self.collision_grid = {}
+        self.grid_size = COLLISION_GRID_SIZE
+
+        # Initialize enemy spawn points list
+        self.enemy_spawn_points = []
+
+        # create the map and record enemy spawn points
         self.create_map()
 
         # user interface
         self.ui = UI()
         self.upgrade = Upgrade(self.player)
 
-        # particles
+        # particles and magic
         self.animation_player = AnimationPlayer()
         self.magic_player = MagicPlayer(self.animation_player)
+        self.frame_count = 0
+
+        # Instantiate the enemy spawner with area-based, time-based respawn
+        self.enemy_spawner = EnemySpawner(self, area_size=100, respawn_interval=5000, update_interval=1000)
 
     def create_map(self):
         layouts = {
@@ -55,58 +65,53 @@ class Level:
         for style, layout in layouts.items():
             for row_index, row in enumerate(layout):
                 for col_index, col in enumerate(row):
-                    if col != '-1':
-                        x = col_index * TILESIZE
-                        y = row_index * TILESIZE
-                        if style == 'boundary':
-                            Tile((x, y), [self.obstacle_sprites], 'invisible')
-                        if style == 'grass':
-                            random_grass_image = choice(graphics['grass'])
-                            Tile(
-                                (x, y),
-                                [self.visible_sprites, self.obstacle_sprites, self.attackable_sprites],
-                                'grass',
-                                random_grass_image)
+                    if col == '-1':
+                        continue
 
-                        if style == 'object':
-                            surf = graphics['objects'][int(col)]
-                            Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'object', surf)
+                    x = col_index * TILESIZE
+                    y = row_index * TILESIZE
 
-                        if style == 'entities':
-                            if col == '394':
-                                self.player = Player(
-                                    (x, y),
-                                    [self.visible_sprites],
-                                    self.obstacle_sprites,
-                                    self.create_attack,
-                                    self.destroy_attack,
-                                    self.create_magic)
+                    if style == 'boundary':
+                        Tile((x, y), [self.obstacle_sprites], 'invisible')
+                    elif style == 'grass':
+                        Tile((x, y), [self.visible_sprites, self.obstacle_sprites, self.attackable_sprites],
+                             'grass', choice(graphics['grass']))
+                    elif style == 'object':
+                        surf = graphics['objects'][int(col)]
+                        Tile((x, y), [self.visible_sprites, self.obstacle_sprites],
+                             'object', surf)
+                    elif style == 'entities':
+                        if col == '394':  # Player spawn
+                            self.player = Player(
+                                (x, y), [self.visible_sprites], self.obstacle_sprites,
+                                self.create_attack, self.destroy_attack, self.create_magic)
+                        else:
+                            if col == '390':
+                                monster_name = 'bamboo'
+                            elif col == '391':
+                                monster_name = 'spirit'
+                            elif col == '392':
+                                monster_name = 'raccoon'
                             else:
-                                if col == '390':
-                                    monster_name = 'bamboo'
-                                elif col == '391':
-                                    monster_name = 'spirit'
-                                elif col == '392':
-                                    monster_name = 'raccoon'
-                                else:
-                                    monster_name = 'squid'
-                                Enemy(
-                                    monster_name,
-                                    (x, y),
-                                    [self.visible_sprites, self.attackable_sprites],
-                                    self.obstacle_sprites,
-                                    self.damage_player,
-                                    self.trigger_death_particles,
-                                    self.add_exp)
+                                monster_name = 'squid'
+                            # Create enemy for initial placement
+                            Enemy(
+                                monster_name,
+                                (x, y),
+                                [self.visible_sprites, self.attackable_sprites],
+                                self.obstacle_sprites,
+                                self.damage_player,
+                                self.trigger_death_particles,
+                                self.add_exp)
+                            # Record spawn point for area-based respawning
+                            self.enemy_spawn_points.append((monster_name, (x, y)))
 
     def create_attack(self):
-
         self.current_attack = Weapon(self.player, [self.visible_sprites, self.attack_sprites])
 
     def create_magic(self, style, strength, cost):
         if style == 'heal':
             self.magic_player.heal(self.player, strength, cost, [self.visible_sprites])
-
         if style == 'flame':
             self.magic_player.flame(self.player, cost, [self.visible_sprites, self.attack_sprites])
 
@@ -138,60 +143,46 @@ class Level:
             self.animation_player.create_particles(attack_type, self.player.rect.center, [self.visible_sprites])
 
     def trigger_death_particles(self, pos, particle_type):
-
-        self.animation_player.create_particles(particle_type, pos, self.visible_sprites)
+        self.animation_player.create_particles(particle_type, pos, [self.visible_sprites])
 
     def add_exp(self, amount):
-
         self.player.exp += amount
 
     def toggle_menu(self):
-
         self.game_paused = not self.game_paused
 
     def run(self):
         self.visible_sprites.custom_draw(self.player)
         self.ui.display(self.player)
-
         if self.game_paused:
             self.upgrade.display()
         else:
             self.visible_sprites.update()
             self.visible_sprites.enemy_update(self.player)
             self.player_attack_logic()
-
+            # Update enemy respawn based on area and time intervals
+            self.enemy_spawner.update()
 
 class YSortCameraGroup(pygame.sprite.Group):
     def __init__(self):
-
-        # general setup
         super().__init__()
         self.display_surface = pygame.display.get_surface()
         self.half_width = self.display_surface.get_size()[0] // 2
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
-
-        # creating the floor
         self.floor_surf = pygame.image.load('../graphics/tilemap/ground.png').convert()
         self.floor_rect = self.floor_surf.get_rect(topleft=(0, 0))
 
     def custom_draw(self, player):
-
-        # getting the offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
-
-        # drawing the floor
         floor_offset_pos = self.floor_rect.topleft - self.offset
         self.display_surface.blit(self.floor_surf, floor_offset_pos)
-
-        # for sprite in self.sprites():
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_pos)
 
     def enemy_update(self, player):
-        enemy_sprites = [sprite for sprite in self.sprites() if
-                         hasattr(sprite, 'sprite_type') and sprite.sprite_type == 'enemy']
+        enemy_sprites = [sprite for sprite in self.sprites() if hasattr(sprite, 'sprite_type') and sprite.sprite_type == 'enemy']
         for enemy in enemy_sprites:
             enemy.enemy_update(player)
